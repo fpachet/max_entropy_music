211482c1eae01a0e882e2dc45b51ce39100ed0f9
Francois Pachet <pachet@gmail.com>
2025-04-16 12:53:40 +0200

    remove useless chord_sequences.txt

--- examples/chord-seq.py
+++ examples/chord-seq.py
@@ -20,14 +20,14 @@ from datasets import load_dataset
 """
 
 
-def download_chord_dataset():
-    ds = load_dataset("ohollo/lmd_chords")
-    chords_list = ds["train"]["chords"]
-    with open("./data/chords/chord_sequences.txt", "w") as file:
-        for chord_seq in chords_list:
-            # If chord_seq is a list, join the chords with a space
-            chords = "; ".join(chord_seq["symbol"])
-            file.write(chords + "\n")
+# def download_chord_dataset():
+#     ds = load_dataset("ohollo/lmd_chords")
+#     chords_list = ds["train"]["chords"]
+#     with open("./data/chords/chord_sequences.txt", "w") as file:
+#         for chord_seq in chords_list:
+#             # If chord_seq is a list, join the chords with a space
+#             chords = "; ".join(chord_seq["symbol"])
+#             file.write(chords + "\n")
 
 
 if __name__ == "__main__":
