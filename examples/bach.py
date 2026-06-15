adf8d6c90d62373cccb30339eab66581c1832f48
Francois Pachet <pachet@gmail.com>
2026-06-15 14:36:08 +0200

    Add Bach interval generation example

--- examples/bach.py
+++ examples/bach.py
@@ -16,6 +16,9 @@ logging.basicConfig(level=logging.INFO)
 
 if __name__ == "__main__":
     midi_pitches = extract_pitches_from_midi(Path("data/midi/bach_partita_violin.mid"))
-    g = SequenceGenerator(midi_pitches, k_max=1, fast=True).train(max_iter=10)
-    for i in range(10):
-        save_midi(g.sample_seq(100), Path(f"generated/bach-partita-{i}.mid"))
+    # midi_pitches = extract_pitches_from_midi(Path("data/midi/bach_prelude_c.mid"))
+    # for i in range(1, 11):
+    #     g = SequenceGenerator(midi_pitches, k_max=i, fast=True).train(max_iter=10)
+    #     save_midi(g.sample_seq(300), Path(f"generated/bach_partita_violin_k{i}.mid"))
+    g = SequenceGenerator(midi_pitches, k_max=10, fast=True).train(max_iter=10)
+    save_midi(g.sample_seq(300), Path(f"generated/bach_partita.mid"))
